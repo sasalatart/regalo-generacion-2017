191cb83f0bd9a515d9a73e9f987c7d9c2eb51457
Sebastián Salata Ruiz-Tagle <sa.salatart@gmail.com>
2018-07-17 21:40:47 -0400

    ✨ update mentoring content

--- src/lib/mentoring.js
+++ src/lib/mentoring.js
@@ -23,7 +23,7 @@ export const mentoringContents = {
     content: dedent`
       1. Te inscribes rellenando un formulario.
 
-      2. El formulario contendrá preguntas (cortas) acerca de tu motivación por participar en este proyecto, además de los temas que te interesan conversar.
+      2. El formulario contendrá preguntas (cortas) acerca de tu motivación por participar en este proyecto, los temas que te interesan conversar, y una aproximación de en qué momento de este año (o del que viene) podrías participar en esto.
 
       3. Los estudiantes podrán ver tu perfil en una página web, para que podamos hacer un *match*.
 
@@ -55,15 +55,13 @@ export const mentoringContents = {
     content: dedent`
       * Esta **no es una plataforma de reclutamiento**, por lo que se prohíbe registrarse y concretar reuniones de mentoría con el fin de reclutar estudiantes para la propia empresa del ex-estudiante, o para cualquier otra. No obstante, sí se busca que el mentor ofrezca guías, consejos y comparta experiencias para que el estudiante pueda enfrentar mejor sus procesos de búsqueda de práctica y/o primer trabajo. Así mismo, se espera que los estudiantes no utilicen la plataforma para solicitar prácticas o empleo.
 
-      * Durante la inscripción podrás indicar tu disponibilidad para que los estudiantes te contacten durante los meses o semanas que más te acomoden.
-
       * Una vez que se hace el calce entre un estudiante y un mentor, se les enviará un correo electrónico para que puedan coordinar el primer encuentro. **Es deber del estudiante tomar la iniciativa para coordinar el primer encuentro**. Para esto debe enviar un email al mentor dentro de los primeros 10 días desde el calce.
 
       * Creemos que un encuentro tipo tiene una duración de 45 min a una hora. Sin embargo, esto dependerá de ustedes y de la conversación que tengan.
 
       * Si el ex-estudiante o el estudiante tiene un imprevisto, pueden acordar un nuevo encuentro, siempre que se notifique durante el día anterior o el mismo día. Si un estudiante o ex-estudiante cancela dos veces un mismo encuentro, entonces el equipo del programa evaluará su continuidad (entendiendo cancelar como dar aviso de inasistencia el día anterior o el mismo día del encuentro). Si un ex-estudiante o estudiante no asiste a un encuentro sin avisar ni dar explicación a la contraparte, será retirado del programa.
 
-      * Tu nombre, motivaciones y tema a conversar aparecerán en una página web a la que tendrán acceso los estudiantes para explorar los distintos perfiles de personas con quienes pueden pedir mentorías. Esta información estará en la página por seis meses.
+      * Tu nombre, motivaciones y tema a conversar aparecerán en una página web a la que tendrán acceso los estudiantes para explorar los distintos perfiles de personas con quienes pueden pedir mentorías. Esta información estará en la página por seis meses aproximadamente.
 
       * Las reuniones deben realizarse en un lugar público, como un café.
     `,
